eec832ff3e37fb4874cda0f5923caecc26ef7337
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-07-03 11:05:41 +1000

    Added getOwnerEmail functionality.

--- src/main/java/au/gov/vic/ecodev/mrt/dao/TemplateConfigDao.java
+++ src/main/java/au/gov/vic/ecodev/mrt/dao/TemplateConfigDao.java
@@ -3,4 +3,6 @@ package au.gov.vic.ecodev.mrt.dao;
 public interface TemplateConfigDao {
 
 	String getTemplateClasses(final String templateName);
+	
+	String getOwnerEmails(final String templateName);
 }
--- src/main/java/au/gov/vic/ecodev/mrt/dao/TemplateConfigDaoImpl.java
+++ src/main/java/au/gov/vic/ecodev/mrt/dao/TemplateConfigDaoImpl.java
@@ -12,21 +12,36 @@ import au.gov.vic.ecodev.mrt.common.db.Constants.Strings;
 public class TemplateConfigDaoImpl implements TemplateConfigDao {
 
 	private static final Logger LOGGER = Logger.getLogger(TemplateConfigDaoImpl.class);
-	
+
+	private static final String SELECT_OWNER_EMAIL_SQL = "SELECT OWNER_EMAILS FROM TEMPLATE_CONFIG WHERE TEMPLATE_NAME = ?";
+
+	private static final String SELECT_TEMPLATE_CLASS_SQL = "select class_names from template_config where template_name = ?";
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Override
 	public String getTemplateClasses(String templateName) {
-		String sql = "select class_names from template_config where template_name = ?";
 		String classes = Strings.EMPTY;
 		try {
-				classes = jdbcTemplate.queryForObject(sql, new Object[] {templateName.toUpperCase()}, 
-						String.class);
+			classes = jdbcTemplate.queryForObject(SELECT_TEMPLATE_CLASS_SQL,
+					new Object[] { templateName.toUpperCase() }, String.class);
 		} catch (EmptyResultDataAccessException e) {
 			LOGGER.warn("No template class found for template: " + templateName, e);
 		}
 		return classes;
+	}
+
+	@Override
+	public String getOwnerEmails(String templateName) {
+		String ownerEmails = Strings.EMPTY;
+		try {
+			ownerEmails = jdbcTemplate.queryForObject(SELECT_OWNER_EMAIL_SQL,
+					new Object[] { templateName.toUpperCase() }, String.class);
+		} catch (EmptyResultDataAccessException e) {
+			LOGGER.warn("No template class found for template: " + templateName, e);
+		}
+		return ownerEmails;
 	}
 
 }
