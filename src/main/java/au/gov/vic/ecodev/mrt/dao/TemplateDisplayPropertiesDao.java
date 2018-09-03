2ef23fa10900aa24c0906c6019037fb5a85e6aaa
vicey3n <vicey3n@192.168.56.1>
2018-09-03 14:37:40 +1000

    Refactor for wider use.

---
+++ src/main/java/au/gov/vic/ecodev/mrt/dao/TemplateDisplayPropertiesDao.java
@@ -0,0 +1,8 @@
+package au.gov.vic.ecodev.mrt.dao;
+
+public interface TemplateDisplayPropertiesDao {
+
+	public String getDisplayProperties(final String template);
+	
+	public String getTemplateHeaders(final String template);
+}
